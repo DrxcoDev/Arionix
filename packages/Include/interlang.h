ef5a64f0d46764ef37aa0a36245acc4bd168fc1f
DrxcoDev <156522801>
2024-10-21 19:26:42 +0200

    First commit

---
+++ packages/Include/interlang.h
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <cstring>
+
+// Functions for every systems 
+#define MACOS_a 0x0001
+#define MACOS_b 0x0002
+#define MACOS_c 0x0003
+#define MACOS_d 0x0004
+
+#define LINUX_a 0x0005
+#define LINUX_b 0x0006
+#define LINUX_c 0x0007
+#define LINUX_d 0x0008
+
+#define WIN_a 0x0009
+#define WIN_b 0x0010
+#define WIN_c 0x0011
+#define WIN_d 0x0012
+
+// Function designed code
+
+int get_system_type(const char* os_name) {
+    
+}
